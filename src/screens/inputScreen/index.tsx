43e7728ccb4dd4c775cc4fe46f0fa2ec4c5cf9aa
unknown <gitfecaf@gmail.com>
2024-08-14 09:50:22 -0300

    primeiro-commit

---
+++ src/screens/inputScreen/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+export const InputScreen=()=>{
+    return(
+        <View>
+            <Text>
+                Hello word
+            </Text>
+        </View>
+    
+    )
+
+}
